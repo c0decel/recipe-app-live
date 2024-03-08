bbf92f6bf90e298c517f38adc907e7e55da589a2
c0decel <seismicm0ke@proton.me>
2024-03-08 13:47:30 -0800

    commit

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
