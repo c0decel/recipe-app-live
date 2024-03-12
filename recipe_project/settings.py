a60aca95aa6df24091e7deb4585f21db58601c77
c0decel <seismicm0ke@proton.me>
2024-03-11 19:46:06 -0700

    pckg

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'recipes/static'
 
 
 MEDIA_URL = '/media/'
