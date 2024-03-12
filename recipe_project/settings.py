c595c6f6ff7bce9ec7dd53f540cb8c103d5009ee
c0decel <seismicm0ke@proton.me>
2024-03-11 21:16:58 -0700

    commit

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-   BASE_DIR / 'static'
+   BASE_DIR / 'recipes/static'
 ]
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
