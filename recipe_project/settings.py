3943fd11a2cf8254a0f86ad0c46cce0f95c04d51
c0decel <seismicm0ke@proton.me>
2024-03-11 20:47:50 -0700

    commit

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'recipes/static'
 
 
 MEDIA_URL = '/media/'
