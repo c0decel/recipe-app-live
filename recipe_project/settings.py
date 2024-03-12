738faf367d8529d202530e64a5ad8b6e3170574f
c0decel <seismicm0ke@proton.me>
2024-03-11 18:20:22 -0700

    pckg

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
