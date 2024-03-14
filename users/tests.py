ef8b0ed1dd8caba94b6c1c3c13ae61f4dbf41643
c0decel <seismicm0ke@proton.me>
2024-03-13 18:27:34 -0700

    update

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import Group, Permission
+from users.models import User
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123', name='Test User')
+
+
+    def test_user_creation(self):
+        """Test user creation"""
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.name, 'Test User')
+        self.assertTrue(self.user.check_password('password123'))
